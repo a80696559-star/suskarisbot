f3ba77f53e59558d129d5a791a10e60e96b3f42c
a80696559-star <a80696559@gmail.com>
2026-05-24 14:00:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,14 @@
-from flask import Flask
-from threading import Thread
 from telegram.ext import Updater, CommandHandler
 
-BOT_TOKEN = "8742437806:AAG7AxjpVPGC0IFD4sZi8IB5qzPFI-O4VJw"
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot Running ✅"
-
-def run_web():
-    app.run(host="0.0.0.0", port=10000)
+TOKEN = "8742437806:AAG7AxjpVPGC0IFD4sZi8IB5qzPFI-O4VJw"
 
 def start(update, context):
     update.message.reply_text("Bot Working ✅")
 
 def help_command(update, context):
-    update.message.reply_text("/start\n/help")
+    update.message.reply_text("Commands:\n/start")
 
-Thread(target=run_web).start()
-
-updater = Updater(BOT_TOKEN, use_context=True)
+updater = Updater(TOKEN, use_context=True)
 
 dp = updater.dispatcher
 
