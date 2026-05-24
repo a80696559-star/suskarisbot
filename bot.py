adb10c0f342e14effab5a345fff1889af090ead5
a80696559-star <a80696559@gmail.com>
2026-05-24 13:52:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,6 @@
 from flask import Flask
 from threading import Thread
-
-from telegram import Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes
-)
+from telegram.ext import Updater, CommandHandler
 
 BOT_TOKEN = "8742437806:AAG7AxjpVPGC0IFD4sZi8IB5qzPFI-O4VJw"
 
@@ -16,57 +10,25 @@ app = Flask(__name__)
 def home():
     return "Bot Running ✅"
 
-
 def run_web():
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    app.run(host="0.0.0.0", port=10000)
 
+def start(update, context):
+    update.message.reply_text("Bot Working ✅")
 
-async def start(
-    update: Update,
-    context: ContextTypes.DEFAULT_TYPE
-):
-    await update.message.reply_text(
-        "Bot Working ✅"
-    )
+def help_command(update, context):
+    update.message.reply_text("/start\n/help")
 
+Thread(target=run_web).start()
 
-async def help_command(
-    update: Update,
-    context: ContextTypes.DEFAULT_TYPE
-):
-    await update.message.reply_text(
-        "/start\n/help"
-    )
+updater = Updater(BOT_TOKEN, use_context=True)
 
+dp = updater.dispatcher
 
-def main():
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help_command))
 
-    application = (
-        Application.builder()
-        .token(BOT_TOKEN)
-        .build()
-    )
+print("BOT STARTED ✅")
 
-    application.add_handler(
-        CommandHandler("start", start)
-    )
-
-    application.add_handler(
-        CommandHandler("help", help_command)
-    )
-
-    print("BOT STARTED ✅")
-
-    Thread(
-        target=run_web,
-        daemon=True
-    ).start()
-
-    application.run_polling()
-
-
-if __name__ == "__main__":
-    main()
+updater.start_polling()
+updater.idle()
